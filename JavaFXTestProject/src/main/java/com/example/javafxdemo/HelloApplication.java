4da58be3a2998ba6899a203e7dfbac68023ed5c7
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-10-29 17:29:53 -0700

    Created login screen logic + home screen

--- JavaFXTestProject/src/main/java/com/example/javafxdemo/HelloApplication.java
+++ JavaFXTestProject/src/main/java/com/example/javafxdemo/HelloApplication.java
@@ -4,19 +4,29 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    private static Stage stg;
+
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage primaryStage) throws IOException {
         // Calling HelloApplication.class here so we need to ensure the fxml file is in the same package as the 
-        // HelloApplication file 
+        // HelloApplication file
+        stg = primaryStage;
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 300, 275);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        Scene scene = new Scene(fxmlLoader.load(), 680, 400);
+        primaryStage.setTitle("Hello!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public void changeScene(String fxml) throws IOException {
+        Parent pane = FXMLLoader.load(getClass().getResource(fxml));
+        stg.getScene().setRoot(pane);
     }
 
     public static void main(String[] args) {
