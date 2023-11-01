a333d5df7496d4e7929afd83392202e609625c5e
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-10-31 22:27:01 -0700

    Added logic for checking role for login

--- JavaFXTestProject/src/main/java/com/example/javafxdemo/Login.java
+++ JavaFXTestProject/src/main/java/com/example/javafxdemo/Login.java
@@ -3,10 +3,7 @@ package com.example.javafxdemo;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import java.sql.Connection;
@@ -30,6 +27,9 @@ public class Login {
     @FXML
     private PasswordField password;
 
+    @FXML
+    private CheckBox isPropertyManager;
+
 
 
     public void userLogin(ActionEvent event) throws IOException {
@@ -49,45 +49,92 @@ public class Login {
         
         Connection conn = null;
         PreparedStatement stmt = null;
-        
-        try {
-            Class.forName(JDBC_DRIVER);
-            conn = DriverManager.getConnection(DB_URL);
-    
-            String sql = "SELECT * FROM users WHERE email = ? AND password = ?";
-            stmt = conn.prepareStatement(sql);
-            stmt.setString(1, email.getText());
-            stmt.setString(2, password.getText()); 
-            ResultSet rs = stmt.executeQuery();
-    
-            if (rs.next()) {
-                wrongLogin.setText("Success!");
-                HelloApplication m = new HelloApplication();
-                m.changeScene("afterLogin.fxml"); // should probably have a couple of these pages one for users and another for Property managers. 
-            } else if (email.getText().isEmpty() && password.getText().isEmpty()) {
-                wrongLogin.setText("Please enter your data.");
-            } else {
-                wrongLogin.setText("Wrong email or password!");
-            }
-    
-            // Clean up
-            rs.close();
-            stmt.close();
-            conn.close();
-    
-        } catch(Exception e) {
-            e.printStackTrace();
-            wrongLogin.setText("Error connecting to the database.");
-        } finally {
+
+        if (isPropertyManager.isSelected()) {
             try {
-                if(stmt != null) stmt.close();
-            } catch(Exception se2) {}
-            try {
-                if(conn != null) conn.close();
-            } catch(Exception se) {
-                se.printStackTrace();
+                Class.forName(JDBC_DRIVER);
+                conn = DriverManager.getConnection(DB_URL);
+
+                String sql = "SELECT * FROM users WHERE email = ? AND password = ? AND ROLE = PropertyManager";
+                stmt = conn.prepareStatement(sql);
+                stmt.setString(1, email.getText());
+                stmt.setString(2, password.getText());
+                ResultSet rs = stmt.executeQuery();
+
+                if (rs.next()) {
+                    wrongLogin.setText("Success!");
+                    HelloApplication m = new HelloApplication();
+                    m.changeScene("afterLogin.fxml"); // should probably have a couple of these pages one for users and another for Property managers.
+                } else if (email.getText().isEmpty() && password.getText().isEmpty()) {
+                    wrongLogin.setText("Please enter your data.");
+                } else {
+                    wrongLogin.setText("Wrong email or password!");
+                }
+
+                // Clean up
+                rs.close();
+                stmt.close();
+                conn.close();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                wrongLogin.setText("Error connecting to the database.");
+            } finally {
+                try {
+                    if (stmt != null) stmt.close();
+                } catch (Exception se2) {
+                }
+                try {
+                    if (conn != null) conn.close();
+                } catch (Exception se) {
+                    se.printStackTrace();
+                }
             }
+
         }
+        else {
+            try {
+                Class.forName(JDBC_DRIVER);
+                conn = DriverManager.getConnection(DB_URL);
+
+                String sql = "SELECT * FROM users WHERE email = ? AND password = ? AND ROLE = Customer";
+                stmt = conn.prepareStatement(sql);
+                stmt.setString(1, email.getText());
+                stmt.setString(2, password.getText());
+                ResultSet rs = stmt.executeQuery();
+
+                if (rs.next()) {
+                    wrongLogin.setText("Success!");
+                    HelloApplication m = new HelloApplication();
+                    m.changeScene("afterLogin.fxml"); // should probably have a couple of these pages one for users and another for Property managers.
+                } else if (email.getText().isEmpty() && password.getText().isEmpty()) {
+                    wrongLogin.setText("Please enter your data.");
+                } else {
+                    wrongLogin.setText("Wrong email or password!");
+                }
+
+                // Clean up
+                rs.close();
+                stmt.close();
+                conn.close();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                wrongLogin.setText("Error connecting to the database.");
+            } finally {
+                try {
+                    if (stmt != null) stmt.close();
+                } catch (Exception se2) {
+                }
+                try {
+                    if (conn != null) conn.close();
+                } catch (Exception se) {
+                    se.printStackTrace();
+                }
+            }
+
+        }
+
     }
 
 
