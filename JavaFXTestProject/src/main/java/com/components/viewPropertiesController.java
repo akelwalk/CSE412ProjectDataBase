7b45ce3eb7b62f212896188d304d59bdda87266c
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-11-11 21:26:31 -0700

    Created ListView and Home Button with IDs

---
+++ JavaFXTestProject/src/main/java/com/components/viewPropertiesController.java
@@ -0,0 +1,23 @@
+package com.components;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.db.database_controller;
+import com.main.MainApplication;
+
+import java.io.IOException;
+import javafx.scene.control.ListView;
+
+
+public class viewPropertiesController {
+
+}
