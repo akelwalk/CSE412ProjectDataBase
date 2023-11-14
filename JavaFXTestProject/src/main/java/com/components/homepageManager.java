b50960c71b5bdc3c6126d261278c8cd4adde0448
Sean Berrios <berrios96sean@gmail.com>
2023-11-13 18:26:31 -0700

    updating property management view for interactions

--- JavaFXTestProject/src/main/java/com/components/homepageManager.java
+++ JavaFXTestProject/src/main/java/com/components/homepageManager.java
@@ -12,11 +12,30 @@ public class homepageManager {
 
     @FXML
     private Button logout;
+    private Button properties; 
+    private Button tenants; 
+    private Button leases; 
 
 
     public void userLogOut(ActionEvent event) throws IOException {
         MainApplication m = new MainApplication();
         m.changeScene("/com/resources/hello-view.fxml");
+
+    }
+
+    public void getProperties(ActionEvent event)throws IOException 
+    {
+                MainApplication m = new MainApplication();
+        m.changeScene("/com/resources/hello-view.fxml");
+    }
+
+    public void getTenants(ActionEvent event)throws IOException 
+    {
+
+    }
+
+    public void getLeases(ActionEvent event)throws IOException 
+    {
 
     }
 }
