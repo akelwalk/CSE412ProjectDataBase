12024e9b869ee6d22c54ada2be236a39d625b0e9
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-11-16 11:07:17 -0700

    Adding display information of the property to the property view page's controller

--- JavaFXTestProject/src/main/java/com/components/PropertyViewController.java
+++ JavaFXTestProject/src/main/java/com/components/PropertyViewController.java
@@ -8,24 +8,42 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.text.Text;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 
 public class PropertyViewController implements Initializable {
 
+    //Display Data
+
+    private String amenities;
     private int propertyID;
+    private String address;
+    private String name;
+    private List<String> communityAnnouncements;
+
 
     IDatabaseOperations databaseHandler = database_controller.getInstance();
 
     @FXML
     private Button homeButton;
 
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    @FXML
+    private Text propertyIDText;
 
+    public void initializeValues(int propertyID)
+    {
+        this.propertyID = propertyID;
+        propertyIDText.setText(String.valueOf(this.propertyID));
     }
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+
 
     public void goHome(ActionEvent event) throws IOException {
         System.out.println("Property View -> Customer Home Page");
